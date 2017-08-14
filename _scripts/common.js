6c52b6369f2ec0c835d0d737e3de9a7c81a57f34
Sun1ive <sunliveua@gmail.com>
2017-08-14 23:22:29 +0300

    0.7 + map

--- _scripts/common.js
+++ _scripts/common.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function() {
+	$('.maps').click(function(){
+	$(this).find('iframe').addClass('clicked')
+	}).mouseleave(function(){
+		$(this).find('iframe').removeClass('clicked');
+	});
+});
