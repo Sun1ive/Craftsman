57bfa4e9e5287fd1a0704560972ba20e904e5a4b
Sun1ive <sunliveua@gmail.com>
2017-08-15 15:34:32 +0300

    1.0 update

--- _scripts/common.js
+++ _scripts/common.js
@@ -4,4 +4,23 @@ $(document).ready(function() {
 	}).mouseleave(function(){
 		$(this).find('iframe').removeClass('clicked');
 	});
+
+	$('.hasMenu').click(function(e) {
+		event.preventDefault();
+		$('.subMenu').toggle();
+	});
+
+	$('.header-lowResMenu').click(function() {
+		$('.lowResMenu').toggle();
+	});
+	$('.formButton').click(function() {
+		$('#form').removeClass('hidden');
+	});
+	$('.close').click(function() {
+		$('#form').addClass('hidden');
+	});
+	$('form').submit(function() {
+		event.preventDefault();
+		alert("ajax_request");
+	});
 });
